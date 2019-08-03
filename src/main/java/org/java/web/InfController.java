667e92f8f6bf66e5632f72f91bcd8c384e3dddd0
zd1997 <duankun1997@qq.com>
2019-08-03 14:57:56 +0800

    11111

--- src/main/java/org/java/web/InfController.java
+++ src/main/java/org/java/web/InfController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class InfController {
     @RequestMapping("/init")
     public String login(){
-        return "你是蔡徐坤";
+        return "你是蔡徐坤吧,1222";
     }
 }
